3cb2602a60f55727fe92d4834a9c779f97468c6d
c2501902968 <chengjie@cjdeMacBook-Pro.local>
2018-12-17 09:41:09 +0800

    Initial commit

---
+++ src/com/youmeek/demo/helloworld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * created by me on 2018/12/17
+ */
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+    }
+}
